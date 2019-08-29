403a4baaedd95da45be7debce14b1a95fd186f8c
Yixuan <yuz313@psu.edu>
2019-08-29 13:24:06 -0400

    test

--- Workflow/Workflow/ViewController.swift
+++ Workflow/Workflow/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weAreLabel: UILabel!
+    @IBOutlet weak var pennStateLabel: UILabel!
+    
+    var cheerCount = 0
+    
+    var isEvnCheerCount : Bool = {return cheerCount % 2 == 0}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pennStateLabel.isHidden = true
+        
     }
 
-
+    @IBAction func cheerPressed(_ sender: UIButton) {
+        weAreLabel.isHidden = !isEvnCheerCount
+        pennStateLabel.isHidden = isEvnCheerCount
+        cheerCount += 1
+    }
+    
 }
-
