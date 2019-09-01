cc9e201064eb4ba495dec868f55e41422c1a64e5
Yixuan <yuz313@psu.edu>
2019-09-01 17:37:49 -0400

    Finished the first version of UI design

---
+++ Word Scramble/Word Scramble/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Yixuan Zhang on 9/1/19.
+//  Copyright © 2019 Yixuan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var wordLengthSelector: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
