abefebde89ff175af9fd444da80052a3c3dd1d88
Yixuan <yuz313@psu.edu>
2019-09-14 12:53:48 -0400

    Assignment #3 Pentominoes Finished half of the UI design.

---
+++ Pentominoes/Pentominoes/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Pentominoes
+//
+//  Created by Yixuan Zhang on 9/13/19.
+//  Copyright © 2019 Yixuan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Board0: UIButton!
+    @IBOutlet weak var Board1: UIButton!
+    @IBOutlet weak var Board2: UIButton!
+    @IBOutlet weak var Board3: UIButton!
+    @IBOutlet weak var Board4: UIButton!
+    @IBOutlet weak var Board5: UIButton!
+    @IBOutlet weak var MainBoard: UIImageView!
+    @IBOutlet weak var solve: UIButton!
+    @IBOutlet weak var hint: UIButton!
+    @IBOutlet weak var reset: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
