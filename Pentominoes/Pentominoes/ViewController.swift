9fd03b0bafdc0ccc6d8295ad8aec8562613c1d55
Yixuan <yuz313@psu.edu>
2019-09-14 15:04:01 -0400

    Now can change the playboard

--- Pentominoes/Pentominoes/ViewController.swift
+++ Pentominoes/Pentominoes/ViewController.swift
@@ -8,26 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class 
 
-    @IBOutlet weak var Board0: UIButton!
-    @IBOutlet weak var Board1: UIButton!
-    @IBOutlet weak var Board2: UIButton!
-    @IBOutlet weak var Board3: UIButton!
-    @IBOutlet weak var Board4: UIButton!
-    @IBOutlet weak var Board5: UIButton!
-    @IBOutlet weak var MainBoard: UIImageView!
-    @IBOutlet weak var solve: UIButton!
-    @IBOutlet weak var hint: UIButton!
-    @IBOutlet weak var reset: UIButton!
+
+class ViewController: UIViewController {
+    let colors=Colors()
+    @IBOutlet weak var mainBoard: UIImageView!
+    @IBOutlet var boards: [UIButton]!
     
+    @IBOutlet weak var elementArea: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        resetBoardsButton()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeBoard(_ sender: UIButton) {
+        let index :Int = sender.tag
+        mainBoard.image = UIImage(named: "Board"+"\(index)")
+        resetBoardsButton()
+        setBoardButton(index: index)
+    }
+    
+    func resetBoardsButton(){
+        for index in 0...5{
+            boards[index].layer.cornerRadius=7
+            boards[index].layer.masksToBounds=true
+            boards[index].layer.borderWidth=4
+            boards[index].layer.borderColor=colors.edgeBlue.cgColor
+        }
+    }
+    
+    func setBoardButton(index: Int){
+        boards[index].layer.borderColor=colors.edgeRed.cgColor
+        
+    }
+    
+    
+    
+    
+    
 }
+
+
+
 
